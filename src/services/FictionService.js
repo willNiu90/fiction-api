37016bceabeab8f74b427bc8377b6872d8a9c3fc
will <will@qq.com>
2020-07-12 18:44:29 +0800

    update

--- src/services/FictionService.js
+++ src/services/FictionService.js
@@ -2,14 +2,27 @@ const FictionModel = require('../models/fiction')
 
 class FictionService {
   static async createFiction(data) {
+    console.log('进入创建')
+    console.time('create')
     const res = await FictionModel.create(data)
+    console.timeEnd('create')
     return res
   }
   static async getFictions() {
     console.log('进入service')
     console.time('start')
-    const res = await FictionModel.find()
+    const res = await FictionModel.find({}, null, {
+      skip: 40,
+      limit: 10,
+      sort: {
+        createdAt: 1,
+      },
+    })
     console.timeEnd('start')
+    console.time('start11')
+    const count = await FictionModel.estimatedDocumentCount({})
+    console.timeEnd('start11')
+    console.log(11111, res.length, count)
     return res
   }
   static async getFiction(_id) {
