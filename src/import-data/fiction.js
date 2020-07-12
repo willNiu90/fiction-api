ea8560f200929c702d7a49cafda9ab4237eeeb14
will <will@qq.com>
2020-07-12 18:43:02 +0800

    update

--- src/import-data/fiction.js
+++ src/import-data/fiction.js
@@ -1,14 +1,21 @@
 const fs = require('fs')
 const axios = require('axios')
 
+console.time('start')
+
 fs.readFile('fiction.json',async function (err, data) {
   if (err) {
       return console.error(err);
   }
+  console.timeEnd('start')
+  console.time('start1')
   let str = data.toString()
   let arr = JSON.parse(str)
-  axios.post('http://127.0.0.1:3000/api/v1/fictions', arr)
-  .then(res => console.log(res))
+  axios.post('http://47.100.7.150:3000/api/v1/fictions', arr)
+  .then(res => {
+    console.timeEnd('start1')
+    console.log('success')
+  })
   .catch(e => {
     console.log('error', e)
   })
